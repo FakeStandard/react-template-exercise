705d6b29ab7a07dc574b765370132a3752fc0223
Riley Lin <rileylin.space@gmail.com>
2022-06-29 11:14:25 +0800

    set up custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, createTheme, CssBaseline, Paper, ThemeProvider, Typography } from "@mui/material";
+
+// set up custom theme(optional)
+
+// define theme
+const theme = createTheme({
+  palette: {
+    primary: { // 主要色調
+      light: "#63b8ff",
+      main: "#0989e3",
+      dark: "#005db0",
+      contrastText: "#000"
+    },
+    secondary: { // 次要色調
+      main: '#4db6ac',
+      light: '#82e9de',
+      dark: '#00867d',
+      contrastText: '#000'
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <Paper elevation={3} sx={{ padding: "1rm", backgroundColor: "secondary.light" }}>
+          <Typography color="primary.dark" variant="h1">
+            Starter App
+          </Typography>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
 
