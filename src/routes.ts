10ffef03942c7a0398445d1ea7a7d001b1ddde12
Riley Lin <rileylin.space@gmail.com>
2022-06-30 12:08:55 +0800

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import { FC } from "react"
 import About from "./pages/About"
 import Home from "./pages/Home"
 import Products from "./pages/Products"
-import Register from "./pages/Register"
 
 interface Route {
     key: string
